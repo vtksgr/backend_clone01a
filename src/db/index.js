09fe416454c5c35b62acdb31d03bbd0130bc9bd9
Sagar Pariyar <vtksgr@gmail.com>
2025-03-24 23:18:56 +0900

    connected to mongo DB

---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose, { connect } from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () =>{
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.Connection.host}`)
+    } catch (error) {
+        console.log("MONGO_DB Connection FAIL!! ", error);
+        process.exit(1)
+    }
+}
+
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import mongoose from "mongoose";
-import  {DB_NAME} from "./constants";
+//require('dotenv').config({path: './env'}); [purano style ho]
+
+import dotenv from "dotenv";
+dotenv.config()
+import connectDB from "./db/index.js"
 import express from "express";
 const app = express();
 
+
+
+
+connectDB()
 //efi
-(async()=>{
+/*(async()=>{
     try {
         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         app.on("error", ()=>{
             console.log("ERROR: ", error);
+            throw error
+        })
+        app.listen(process.env.PORT, ()=>{
+            console.log(`app is listning on port ${process.env.PORT}`)
         })
     } catch (error) {
         console.error("ERROR: ", error)
         throw error;
     }
 })()
+    */
